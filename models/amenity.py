a27b82e953ce697122715b86be43f57872a43f7f
Jenn <jennogden95@gmail.com>
2018-11-13 22:59:29 +0000

    Initial commit

---
+++ models/amenity.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Module for Amenity class
+"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """
+    The Amenity class inherits the following attributes from BaseModel:
+
+    (str) name: empty
+    """
+    name = ""
